b1da92107b900cbc5962bff27262d2bd5fe03753
Anshu kumar <anshu4125kumar@gmail.com>
2024-11-06 02:53:24 +0530

    wumpus animation added to contact us

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,12 +1,11 @@
 import Image from "next/image";
 import line from "@/assets/line1.svg"
-import line2 from "@/assets/line2.svg"
 import Square from "@/assets/BlueSquare.svg"
 import Circle from "@/assets/GreenCircle.svg"
 import MobileLogo from "@/assets/MobileLogo.svg"
 import leftlogo from "@/assets/LeftLogo.svg"
 import rightlogo from "@/assets/RightLogo.svg"
-
+import Animation from "@/components/animation/Animation"
 export default function Events(){
     return(
         <>
@@ -15,8 +14,6 @@ export default function Events(){
       <div><Image src={Square} alt="Element-Square" className="lg:h-1/2 h-1/6 absolute lg:left-0 -left-16 top-[40vh] -z-0"/></div>
       <div><Image src={Circle} alt="Element-Square" className="lg:h-1/2 h-1/6 absolute lg:right-0 -right-16 top-[50vh] -z-0"/></div>
        <div className="absolute -top-4 right-0 -z-10 h-screen w-auto"><Image src={line} alt="line"/></div>
-       <div className="absolute top-[1000px] left-[450px] -z-10 h-[200vh] w-full lg:block hidden"><Image src={line2} alt="line"/></div>
- 
        <div className="w-1/2 m-auto mt-32">
        
        <div className="flex flex-col lg:flex-row justify-center lg:justify-between items-center h-full">
@@ -27,6 +24,8 @@ export default function Events(){
         <div className=" h-52 justify-center w-[100%] hidden lg:flex p-5"><Image src={rightlogo} alt="GDG-Logo"/></div>
     </div>
        </div>
+       <Animation></Animation>
+       
        </div>
        </>
     )
--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -29,4 +29,3 @@ export default function ActionAreaCard() {
     </Card>
   );
 }
-s
