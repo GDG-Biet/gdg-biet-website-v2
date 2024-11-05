2b9627a394b11911498bd6d8656919801e5264d4
= <=>
2024-11-05 16:17:14 +0530

    pull

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -6,6 +6,7 @@ import Circle from "@/assets/GreenCircle.svg"
 import MobileLogo from "@/assets/MobileLogo.svg"
 import leftlogo from "@/assets/LeftLogo.svg"
 import rightlogo from "@/assets/RightLogo.svg"
+import { ProfileForm } from "@/components/Form";
 
 export default function Events(){
     return(
@@ -15,7 +16,7 @@ export default function Events(){
       <div><Image src={Square} alt="Element-Square" className="lg:h-1/2 h-1/6 absolute lg:left-0 -left-16 top-[40vh] -z-0"/></div>
       <div><Image src={Circle} alt="Element-Square" className="lg:h-1/2 h-1/6 absolute lg:right-0 -right-16 top-[50vh] -z-0"/></div>
        <div className="absolute -top-4 right-0 -z-10 h-screen w-auto"><Image src={line} alt="line"/></div>
-       <div className="absolute top-[1000px] left-[450px] -z-10 h-[200vh] w-full lg:block hidden"><Image src={line2} alt="line"/></div>
+       {/* <div className="absolute top-[1000px] left-[450px] -z-10 h-[200vh] w-full lg:block hidden"><Image src={line2} alt="line"/></div> */}
  
        <div className="w-1/2 m-auto mt-32">
        
@@ -28,6 +29,7 @@ export default function Events(){
     </div>
        </div>
        </div>
+       <ProfileForm />
        </>
     )
 }
