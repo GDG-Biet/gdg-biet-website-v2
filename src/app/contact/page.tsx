3ef44bdc280393c6d12fb714e661d869d32973c4
Anshu kumar <anshu4125kumar@gmail.com>
2024-11-11 14:27:03 +0530

    made minor chagnes in layout and created archive section in events

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,38 +1,14 @@
-import Image from "next/image";
-import line from "@/assets/line1.svg"
-import Square from "@/assets/BlueSquare.svg"
-import Circle from "@/assets/GreenCircle.svg"
-import MobileLogo from "@/assets/MobileLogo.svg"
-import leftlogo from "@/assets/LeftLogo.svg"
-import rightlogo from "@/assets/RightLogo.svg"
 import Form from "@/components/Form";
 import Animation from "@/components/animation/Animation"
 export default function Events(){
     return(
         <>
         {/* Hero Section */}
-      <div className="lg:h-[80vh] mb-20">
-      <div><Image src={Square} alt="Element-Square" className="lg:h-1/2 h-1/6 absolute lg:left-0 -left-16 top-[40vh] -z-0"/></div>
-      <div><Image src={Circle} alt="Element-Square" className="lg:h-1/2 h-1/6 absolute lg:right-0 -right-16 top-[50vh] -z-0"/></div>
-       <div className="absolute -top-4 right-0 -z-10 h-screen w-auto"><Image src={line} alt="line"/></div>
-       {/* <div className="absolute top-[1000px] left-[450px] -z-10 h-[200vh] w-full lg:block hidden"><Image src={line2} alt="line"/></div> */}
- 
-       <div className="w-1/2 m-auto mt-32">
-       
-       <div className="flex flex-col lg:flex-row justify-center lg:justify-between items-center h-full">
-        {/* logo for smaller screen */}
-        <div className="lg:hidden h-10"><Image src={MobileLogo} alt="GDG-Logo"/></div> 
-       <div className=" h-52 justify-center w-[100%] hidden lg:flex p-5"><Image src={leftlogo} alt="GDG-Logo"/></div> 
-        <div className="text-center lg:text-6xl text-5xl font-Lato text-[#656C73]"><strong className="text-yellow-500">C</strong>ontact<br/></div>
-        <div className=" h-52 justify-center w-[100%] hidden lg:flex p-5"><Image src={rightlogo} alt="GDG-Logo"/></div>
-    </div>
-       </div>
-       <Animation></Animation>
-       
-       </div>
-       <div className=" flex justify-center w-[90%] bg-blue-500 m-auto p-4 dark:bg-gray-900 dark:bg-opacity-50 backdrop-blur-lg rounded-xl">
+      <div className="h-[30vh]"></div>
+       <div className=" flex justify-center w-[90%] bg-blue-500 m-auto p-4 dark:bg-gray-900 dark:bg-opacity-50 backdrop-blur-lg rounded-xl relative">
         <Form></Form>
        </div>
+       <div className="absolute top-48 w-full flex justify-center"><Animation></Animation></div>
        </>
     )
 }
--- src/app/hackathon/page.tsx
+++ src/app/hackathon/page.tsx
@@ -9,23 +9,46 @@ import rightlogo from "@/assets/RightLogo.svg"
 export default function Hackathon(){
     return(
         <>
-        {/* Hero Section */}
-      <div className="lg:h-[80vh] mb-20">
-      <div><Image src={Square} alt="Element-Square" className="lg:h-1/2 h-1/6 absolute lg:left-0 -left-16 top-[40vh] -z-0"/></div>
-      <div><Image src={Circle} alt="Element-Square" className="lg:h-1/2 h-1/6 absolute lg:right-0 -right-16 top-[50vh] -z-0"/></div>
-       <div className="absolute -top-4 right-0 -z-10 h-screen w-auto"><Image src={line} alt="line"/></div>
- 
-       <div className="w-1/2 m-auto mt-32">
-       
-       <div className="flex flex-col lg:flex-row justify-center lg:justify-between items-center h-full">
-        {/* logo for smaller screen */}
-        <div className="lg:hidden h-10"><Image src={MobileLogo} alt="GDG-Logo"/></div> 
-       <div className=" h-52 justify-center w-[100%] hidden lg:flex p-5"><Image src={leftlogo} alt="GDG-Logo"/></div> 
-        <div className="text-center lg:text-6xl text-4xl font-Lato text-[#656C73]">GDG-Hacks</div>
-        <div className=" h-52 justify-center w-[100%] hidden lg:flex p-5"><Image src={rightlogo} alt="GDG-Logo"/></div>
-    </div>
-       </div>
-       </div>
+       {/* Hero Section */}
+      <div className="lg:h-[90vh] pt-20">
+        <div className="absolute w-full h-full top-0 bg-white dark:bg-black dark:bg-opacity-20 bg-opacity-20 backdrop-blur-sm -z-10"></div>
+        <div>
+          <Image
+            src={Square}
+            alt="Element-Square"
+            className="lg:h-1/2 h-1/6 absolute lg:left-0 -left-16 md:top-[40vh] top-[20vh] -z-0"
+          />
+        </div>
+        <div>
+          <Image
+            src={Circle}
+            alt="Element-Square"
+            className="lg:h-1/2 h-1/6 absolute lg:right-0 -right-16 md:top-[50vh] top-[30vh] -z-0"
+          />
+        </div>
+        <div className="absolute -top-4 right-0 -z-0 h-screen w-auto">
+          <Image src={line} alt="line" />
+        </div>
+
+        <div className="w-1/2 m-auto mt-32">
+          <div className="flex flex-col lg:flex-row justify-center lg:justify-between items-center h-full">
+            {/* logo for smaller screen */}
+            <div className="lg:hidden h-10">
+              <Image src={MobileLogo} alt="GDG-Logo" />
+            </div>
+            <div className=" h-72 justify-center w-[100%] hidden lg:flex p-5">
+              <Image src={leftlogo} alt="GDG-Logo" />
+            </div>
+            <div className="text-center lg:text-9xl text-5xl font-Lato dark:text-white text-gray-400 font-bold">
+              <strong className="text-yellow-500">H</strong>ack
+              <br />
+            </div>
+            <div className=" h-72 justify-center w-[100%] hidden lg:flex p-5">
+              <Image src={rightlogo} alt="GDG-Logo" />
+            </div>
+          </div>
+        </div>
+      </div>
        </>
     )
 }
