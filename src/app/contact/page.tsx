ba416ee27422de80734174eb35ac77e4891c5563
Anshu kumar <anshu4125kumar@gmail.com>
2024-12-05 22:17:46 +0530

    comment the contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,11 +1,9 @@
-import Form from "@/components/Form";
-import Animation from "@/components/animation/Animation"
-export default function Events(){
+// import Form from "@/components/Form";
+// import Animation from "@/components/animation/Animation"
+export default function Contacts(){
     return(
         <>
-        {/* Hero Section */}
-      <div className="h-[30vh]"></div>
-       <div className=" flex justify-center lg:w-[90%] bg-blue-500 m-auto lg:p-4 dark:bg-gray-900 dark:bg-opacity-50 backdrop-blur-lg rounded-xl relative">
+       {/* <div className="mt-32 flex justify-center lg:w-[90%] bg-blue-500 m-auto lg:p-4 dark:bg-gray-900 dark:bg-opacity-50 backdrop-blur-lg rounded-xl relative">
         <Form></Form>
        </div>
        <div className="absolute top-48 w-full flex justify-center"><Animation></Animation></div>
@@ -14,7 +12,7 @@ export default function Events(){
         <div className=" cursor-pointer p-20 flex justify-center items-center text-purple-600 hover:text-purple-400 bg-purple-400 hover:bg-purple-600 rounded-full my-10 duration-300">
             <p className="lg:text-5xl text-3xl text-center font-extrabold"><a href="mailto:bietgdsc2023@gmail.com">Send us a Mail</a></p>
             </div>
-       </div>
+       </div> */}
        </>
     )
 }
